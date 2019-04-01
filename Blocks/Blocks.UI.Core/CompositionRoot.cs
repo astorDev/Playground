8d0cde2fd10396adc5aa08cf5c844e0275870f8e
Егор <Pavluchenk0@yandex.ru>
2019-04-01 17:22:27 +0300

    Добавил UWP проект

---
+++ Blocks/Blocks.UI.Core/CompositionRoot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Blocks.UI.Core.ViewModels;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blocks.UI.Core
+{
+    public static class CompositionRoot
+    {
+        public static IServiceProvider ServiceProvider;
+
+        static CompositionRoot()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<MainViewModel>();
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+    }
+}
